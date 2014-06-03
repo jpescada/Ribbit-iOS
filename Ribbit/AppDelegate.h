b29b2e6693e18079356551b548cd0e505a7d8d76
Joao Pescada <joao.pescada@gmail.com>
2014-06-03 13:47:48 +0100

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Joao Pescada on 03/06/2014.
+//  Copyright (c) 2014 Joao Pescada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Joao Pescada on 03/06/2014.
+//  Copyright (c) 2014 Joao Pescada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
